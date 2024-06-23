47673d43e68e448ad226d9a636f68d4e79dced38
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2024-06-23 14:53:05 -0300

    feat: administração das academias parceiras do app

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+from authentication.models import *
+
 
 # Register your models here.
+class AcademyAdmin(ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 20
+
+
+admin.site.register(Academy, AcademyAdmin)
