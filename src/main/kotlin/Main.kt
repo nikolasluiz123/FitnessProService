1bca5546ea651e715e07f4bace5c9e3d162e9413
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2025-02-08 17:21:55 -0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package br.com
+
+fun main() {
+    println("Hello World!")
+}
---
+++ src/test/kotlin/br/com/fitnesspro/FitnessproApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.fitnesspro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FitnessproApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
