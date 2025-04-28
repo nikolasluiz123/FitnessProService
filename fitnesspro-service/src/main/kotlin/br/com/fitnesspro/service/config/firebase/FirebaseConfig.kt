8743f6699006252236d30a8a37ad671e7ee4c1b1
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2025-04-28 18:30:52 -0300

    fix: ajustes para utilizar a secret contendo o JSON de autenticação

--- fitnesspro-service/src/main/kotlin/br/com/fitnesspro/service/config/firebase/FirebaseConfig.kt
+++ fitnesspro-service/src/main/kotlin/br/com/fitnesspro/service/config/firebase/FirebaseConfig.kt
@@ -4,7 +4,10 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import org.springframework.context.annotation.Configuration
+import java.io.ByteArrayInputStream
+import java.io.File
 import java.io.FileInputStream
+import java.io.InputStream
 import javax.annotation.PostConstruct
 
 @Configuration
@@ -12,7 +15,7 @@ class FirebaseConfig {
 
     @PostConstruct
     fun initializeFirebase() {
-        val serviceAccount = FileInputStream("serviceAccountKey.json")
+        val serviceAccount = getCredentialsInputStream()
 
         val options = FirebaseOptions.builder()
             .setCredentials(GoogleCredentials.fromStream(serviceAccount))
@@ -20,6 +23,17 @@ class FirebaseConfig {
 
         if (FirebaseApp.getApps().isEmpty()) {
             FirebaseApp.initializeApp(options)
+        }
+    }
+
+    private fun getCredentialsInputStream(): InputStream {
+        val file = File("serviceAccountKey.json")
+
+        return if (file.exists()) {
+            FileInputStream(file)
+        } else {
+            val json = System.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+            ByteArrayInputStream(json.toByteArray(Charsets.UTF_8))
         }
     }
 }
