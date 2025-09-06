d1654013a2c07526ec05c0ba4d932e6db249c8ba
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2025-09-06 18:57:20 -0300

    fix: removendo expiração do cache
    Issue #4

--- fitnesspro-service/src/main/kotlin/br/com/fitnesspro/config/application/cache/CacheConfig.kt
+++ fitnesspro-service/src/main/kotlin/br/com/fitnesspro/config/application/cache/CacheConfig.kt
@@ -7,7 +7,6 @@ import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cache.caffeine.CaffeineCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.util.concurrent.TimeUnit
 
 @Configuration
 @EnableCaching
@@ -16,7 +15,6 @@ class CacheConfig {
     @Bean
     fun cacheManager(): CacheManager {
         val caffeine = Caffeine.newBuilder()
-            .expireAfterWrite(30, TimeUnit.SECONDS)
             .maximumSize(1000)
 
         return CaffeineCacheManager(*cacheNames).apply {
